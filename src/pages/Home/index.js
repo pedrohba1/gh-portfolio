1563364d3434418003394bbad1c9dff8ac04b031
Pedro Bufulin <pedrohba18@gmail.com>
2020-07-09 22:14:38 -0300

    homepage video background

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-// import { Container } from './styles';
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import PixPlugin from "gsap/PixiPlugin";
+
+import LazyLoad from "react-lazy-load";
+import { VideoContainer, Container, Video, Overlay } from "./styles";
+
+import banner from "~/assets/banner.mp4";
 
 function Home() {
+    let videoRef = useRef(null);
+
+    useEffect(() => {
+        gsap.registerPlugin(PixPlugin);
+        gsap.from(videoRef, 1, { webkitFilter: "brightness(0    %)" });
+        gsap.to(videoRef, 3, { webkitFilter: "brightness(100%)" });
+    });
+
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <Container>
+            <VideoContainer
+                ref={(el) => {
+                    videoRef = el;
+                }}
+            >
+                <Video loop autoPlay>
+                    <source src={banner} type="video/mp4" />
+                    <source src={banner} type="video/ogg" />
+                    <track kind="subtitles" srcLang="en" label="English" />
+                    Your browser does not support the video tag.
+                </Video>
+
+                <Overlay>
+                    <h1>teste</h1>
+                </Overlay>
+            </VideoContainer>
+        </Container>
     );
 }
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,44 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-  
+    z-index: 1;
+
+    width: 100%;
+    height: 100%;
+    text-align: center;
 `;
+
+export const Banner = styled.div`
+    width: 100%;
+    background: #444;
+`;
+
+export const VideoContainer = styled.div`
+    z-index: 0;
+    display: flex;
+`;
+
+export const Overlay = styled.div`
+    display: flex;
+
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+
+    h1 {
+        align-self: center;
+        color: #fff;
+    }
+`;
+
+export const Video = styled.video`
+    width: 100%;
+    display: block;
+`;
+
+export const Navbar = styled.div``;
